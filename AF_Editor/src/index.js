3fb078da41259a26551bdf44361d7d53fde5134e
allenkay76 <allenkay76@protonmail.com>
2023-03-11 15:25:33 +1000

    EDIT: Added top bar menu items

--- AF_Editor/src/index.js
+++ AF_Editor/src/index.js
@@ -1,10 +1,108 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, MenuItem} = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
+
+const template = [
+  {
+  label: 'File',
+  submenu:[
+    {
+      label: 'New',
+      role: 'New'
+    },
+    {
+      label: 'Open',
+      role: 'Open'
+    },
+    {
+      label: 'Save',
+      role: 'Save'
+    },{
+      label: 'Quit',
+      role: 'Quit'
+    }
+  ],
+  
+},
+{
+  label: 'Edit',
+  submenu: [
+     {
+        role: 'undo'
+     },
+     {
+        role: 'redo'
+     },
+     {
+        type: 'separator'
+     },
+     {
+        role: 'cut'
+     },
+     {
+        role: 'copy'
+     },
+     {
+        role: 'paste'
+     }
+  ]
+},
+
+{
+  label: 'View',
+  submenu: [
+     {
+        role: 'reload'
+     },
+     {
+        role: 'toggledevtools'
+     },
+     {
+        type: 'separator'
+     },
+     {
+        role: 'resetzoom'
+     },
+     {
+        role: 'zoomin'
+     },
+     {
+        role: 'zoomout'
+     },
+     {
+        type: 'separator'
+     },
+     {
+        role: 'togglefullscreen'
+     }
+  ]
+},
+
+{
+  role: 'window',
+  submenu: [
+     {
+        role: 'minimize'
+     },
+     {
+        role: 'close'
+     }
+  ]
+},
+
+{
+  role: 'help',
+  submenu: [
+     {
+        label: 'Learn More'
+     }
+  ]
+}
+]
 
 const createWindow = () => {
   // Create the browser window.
@@ -20,14 +118,24 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 app.on('ready', createWindow);
 
+/*
+const menu = Menu.buildFromTemplate(template)
+
+app.on('ready', () => {
+  Menu.setAppApplicationMenu(menu);
+  createWindow();
+})
+*/
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
@@ -47,3 +155,5 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
